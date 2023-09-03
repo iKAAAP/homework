9fe3f3d741faa42fdadb5313d6e582d013194a61
iKAAAP <118036491+iKAAAP@users.noreply.github.com>
2023-09-03 07:59:09 +0400

    закомментировал первую задачу

--- dz.py
+++ dz.py
@@ -11,5 +11,4 @@ n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во 
 print(n_set)
 m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
 print(m_set)
-s_set = sorted(n_set.intersection(m_set))
-print(*s_set)
+s_set = sorted(n_set.intersection(m_set))print(*s_set)
